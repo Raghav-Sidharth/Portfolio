adabc0631a4bd96a77759e04e2c852cef2fdf6e3
Raghav Sidharth <raghav.sidharth@live.com>
2023-05-02 17:29:23 +1200

    Complted hard code front end

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,14 @@
 import Hero from './Hero'
 import NavBar from './NavBar'
 import About from './About'
-
+import Portfolio from './Portfolio'
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
       <About />
+      <Portfolio />
     </>
   )
 }

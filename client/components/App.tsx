a83be0758bfd3f4a0345c5398652c21168830c7b
Raghav Sidharth <raghav.sidharth@live.com>
2023-05-02 15:31:28 +1200

    HEr image now has CSS!

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,7 @@
+import Hero from './Hero'
+
 function App() {
-  return (
-    <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
-    </div>
-  )
+  return <Hero />
 }
 
 export default App
---
+++ client/components/Hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <div>
+      <div className="Hero-Container">
+        <header>
+          <div className="Hero-Container-Content">
+            <div className="Hero-Container-Content-Text">
+              <h1>Hello there,</h1>
+              <p>I&apos;m Raghav Sidharth</p>
+              <p>A Full Stack Web Developer based in Auckland New Zealand</p>
+            </div>
+            <img className="Logo-Hero" src="images/Logo.png" alt="" />
+          </div>
+        </header>
+      </div>
+    </div>
+  )
+}

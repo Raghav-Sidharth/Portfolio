6e31bd5fed3820e3448de38b9d3902561da1b2c3
Raghav Sidharth <raghav.sidharth@live.com>
2023-05-03 12:48:54 +1200

    Added default server path

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
 import { join } from 'node:path'
 import express from 'express'
+import { resolve } from 'node:path'
 
 const server = express()
 
 server.use(express.static(join(__dirname, 'public')))
+server.get('*', (req, res) => {
+  res.sendFile(resolve('server/public/index.html'))
+})
 
 export default server
